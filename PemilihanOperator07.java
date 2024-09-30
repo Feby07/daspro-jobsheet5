ab034086edfa6de833adb110c511f4b9ce1e2222
Feby07 <rahmawatifeby5@gmail.com>
2024-09-30 22:19:37 +0700

    commit

---
+++ PemilihanOperator07.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class PemilihanOperator07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double angka1, angka2, hasil = 0;
+        char operator;
+
+        System.out.println("Masukkan angka 1: ");
+        angka1 = sc.nextDouble();
+        System.out.println("Masukkan angka 2: ");
+        angka2 = sc.nextDouble();
+        System.out.print("Masukkan operator (+ - * :)");
+        operator = sc.next().charAt(0);
+
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+        } else if (operator == '/') {
+            hasil = angka1 / angka2;
+        } else {
+            System.out.println("Operator tidak valid. Silahkan pilih operator +, -, *, atau /.");
+            return;
+        }
+
+        System.out.println(angka1 + " " + operator + " " + angka2 + " = " + hasil);
+
+    }
+}
